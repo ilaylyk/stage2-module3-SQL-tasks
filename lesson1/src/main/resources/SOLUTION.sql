0c241f62d5558205a211e811afc05a48de6ed822
ilaylyk <ilay_lyk@mail.ru>
2022-12-15 03:36:45 +0300

    sql

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,2 +1,6 @@
-
+create table Student(id bigint primary key, name varchar(255), birthday date, "group" int);
+create table Subject(id bigint primary key, name varchar(255), description varchar(255), grade int);
+create table PaymentType(id bigint primary key, name varchar(255));
+create table Payment(id bigint primary key, type_id bigint references PaymentType(id), amount decimal, student_id bigint references Student(id), payment_date datetime);
+create table Mark(id bigint primary key, student_id bigint references Student(id), subject_id bigint references Subject(id), mark int);
 
---
+++ lesson4/src/main/resources/SOLUTION.sql
@@ -0,0 +1,4 @@
+SELECT name, birthday, groupnumber FROM student;
+SELECT * FROM Student LIMIT 50;
+SELECT name FROM Student;
+SELECT DISTINCT amount FROM PAYMENT;
