67e6bac1037e323bb101ab5ab4bdc356e565f0c4
ilaylyk <ilay_lyk@mail.ru>
2022-12-27 00:18:37 +0300

    updat 3

--- lesson2/src/main/resources/SOLUTION.sql
+++ lesson2/src/main/resources/SOLUTION.sql
@@ -1,43 +1,41 @@
-INSERT INTO student (name, birthday, groupnumber)
-VALUES ('John', current_date, 1),
-       ('Chris', current_date, 1),
-       ('Carl', current_date, 1),
-       ('Oliver', current_date, 2),
-       ('James', current_date, 2),
-       ('Lucas', current_date, 2),
-       ('Henry', current_date, 2),
-       ('Jacob', current_date, 3),
-       ('Logan', current_date, 3),
-       ('Ilay', current_date, 4),
-       ('Ronaldo', current_date, 5);
+insert into student (name, groupnumber) values ('John', 1);
+insert into student (name, groupnumber) values ('Chris', 1);
+insert into student (name, groupnumber) values ('Carl', 1);
+insert into student (name, groupnumber) values ('Oliver', 2);
+insert into student (name, groupnumber) values ('James', 2);
+insert into student (name, groupnumber) values ('Lucas', 2);
+insert into student (name, groupnumber) values ('Henry', 2);
+insert into student (name, groupnumber) values ('Jacob', 3);
+insert into student (name, groupnumber) values ('Logan', 3);
+insert into student (name, groupnumber) values ('Paul', 4);
+insert into student (name, groupnumber) values ('Nadezhda', 4);
+insert into student (name, groupnumber) values ('Anna', 5);
+insert into student (name, groupnumber) values ('Kseniya', 5);
 
-INSERT INTO subject (name, description, grade)
-VALUES ('Art', 'interesting subject', 1),
-       ('music', 'interesting subject', 1),
-       ('Geography', 'interesting subject', 2),
-       ('history', 'interesting subject', 2),
-       ('PE', 'interesting subject', 3),
-       ('math', 'interesting subject', 3),
-       ('Science', 'interesting subject', 4),
-       ('IT', 'interesting subject', 4),
-       ('Biology', 'interesting subject', 5),
-       ('Chemistry', 'interesting subject', 5);
+insert into subject (name, grade) values ('Art', 1);
+insert into subject (name, grade) values ('Music', 1);
+insert into subject (name, grade) values ('Geography', 2);
+insert into subject (name, grade) values ('History', 2);
+insert into subject (name, grade) values ('PE', 3);
+insert into subject (name, grade) values ('Math', 3);
+insert into subject (name, grade) values ('Science', 4);
+insert into subject (name, grade) values ('IT', 4);
+insert into subject (name, grade) values ('Economics', 5);
+insert into subject (name, grade) values ('Sociology', 5);
 
+insert into paymenttype (name) values('DAILY');
+insert into paymenttype (name) values('WEEKLY');
+insert into paymenttype (name) values('MONTHLY');
 
-INSERT INTO paymenttype (name)
-VALUES ('DAILY'), ('WEEKLY'), ('MONTHLY');
+insert into payment (amount, student_id, type_id) values (333.33, 1, 2);
+insert into payment (amount, student_id, type_id) values (222.33, 4, 3);
+insert into payment (amount, student_id, type_id) values (444.33, 7, 2);
+insert into payment (amount, student_id, type_id) values (111.33, 5, 1);
+insert into payment (amount, student_id, type_id) values (555.33, 9, 3);
 
-INSERT INTO payment (type_id, amount, payment_date, student_id)
-VALUES (2, 1, '2020-02-07 08:09:00', 1),
-       (3, 1, '2020-02-07 08:09:00', 4),
-       (2, 1, '2020-02-07 08:09:00', 7),
-       (1, 1, '2020-02-07 08:09:00', 5),
-       (1, 10, '2020-02-07 08:09:00', 10);
-
-INSERT INTO mark (student_id, subject_id, mark)
-VALUES (2, 1, 8),
-       (4, 4, 5),
-       (5, 3, 9),
-       (8, 6, 4),
-       (9, 5, 9),
-       (11, 1, 7);
+insert into mark (student_id, subject_id, mark) values (2, 1, 8);
+insert into mark (student_id, subject_id, mark) values (4, 4, 5);
+insert into mark (student_id, subject_id, mark) values (5, 3, 9);
+insert into mark (student_id, subject_id, mark) values (8, 6, 4);
+insert into mark (student_id, subject_id, mark) values (9, 5, 9);
+insert into mark (student_id, subject_id, mark) values (3, 2, 7);
